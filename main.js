ea1a59407a1e8fccdbfbc197e85b783cb3ecf84e
MarianFava <marianaspano@gmail.com>
2021-10-19 23:46:22 -0300

    Desafio 6 Incorporar Arrays

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-//En esta variable de acumula todo lo que se agrega al carrito//
+//En esta variable se acumula todo lo que se agrega al carrito//
+let carrito = [];
 let totalCarrito = 0;
 let totalConDescuento = 0;
+
 //Declaración de clase producto para creación de Macetas
 class Producto {
     constructor(nombre, precio, color, tamanio, porcDescuento = 0) {
@@ -12,67 +14,87 @@ class Producto {
     }
     //Método para calcular descuento//
     descuento() {
-        let descuento = this.precio
+        return this.precio * this.porcDescuento/100;
     }
-    aplicarDescuento(code) {
-        let precioConDescuento = this.precio;
-        //Código de descuento a ingresar//
-        if (code == "descuento10") {
-            precioConDescuento = this.precio * (100 - this.porcDescuento) / 100;
-        }
-
-        return precioConDescuento;
+    aplicarDescuento() {
+        //Precio con descuento aplicado//
+        return this.precio * (100 - this.porcDescuento) / 100;
     }
 }
-//Instanciación de objetos tipo Producto(distintas macetas)
-let productoBuda = new Producto("Maceta Buda", 300, "Dorado", "Mediano", 0);
-let productoGatito = new Producto("Maceta Gatito", 300, "Gris", "Mediano", 10);
-let productoCactus = new Producto("Maceta Cactus", 200, "Verde", "Pequeño",0);
-let productoLlama = new Producto("Maceta Llama", 300, "Turquesa", "Mediano");
-//"Maceta Buda", etc, nombre de los productos a ingresar//
-function agregarAlCarrito() {
-    do {
-        let producto = prompt("Ingresar producto");
-        let precio = 0;
-        let precioConDescuento = 0;
-        let discount = "";
-        switch (producto) {
-            case productoBuda.nombre:
-                precio = productoBuda.precio;
-                discount = prompt("Ingrese código de descuento o Enter para continuar");
-                precioConDescuento = productoBuda.aplicarDescuento(discount);
-                break;
-            case productoGatito.nombre:
-                precio = productoGatito.precio;
-                discount = prompt("Ingrese código de descuento o Enter para continuar");
-                precioConDescuento = productoGatito.aplicarDescuento(discount);
-                break;
-            case productoCactus.nombre:
-                precio = productoCactus.precio;
-                discount = prompt("Ingrese código de descuento o Enter para continuar");
-                precioConDescuento = productoCactus.aplicarDescuento(discount);
-                break;
-            case productoLlama.nombre:
-                precio = productoLlama.precio;
-                discount = prompt("Ingrese código de descuento o Enter para continuar");
-                precioConDescuento = productoLlama.aplicarDescuento(discount);
-                break;
-            default:
-                alert("El producto no existe.");
+
+//Se define una clase para representar los items en carrito
+class ItemCarrito{
+    constructor(producto){
+        this.producto = producto;
+        this.cantidad = 0;
+    }
+
+    incrementar(cantidad){
+        this.cantidad += cantidad;
+    }
+
+    decrementar(){
+        if (this.cantidad > 0 ){
+            this.cantidad--;            
+        }
+    }
+
+}
+//Funcion que permite seleccionar productos para agregar al carrito
+function agregarAlCarrito(){
+    let otroProducto;
+    let producto;
+    let cantidad;
+    let productoSeleccionado;
+    
+    do{
+        //Se ingresa producto seleccionado
+        producto = prompt("Ingrese el producto");
+        //Se busca el producto seleccionado en el array de Items(ItemCarrito)
+        productoSeleccionado = carrito.find( itemCarrito => itemCarrito.producto.nombre == producto) ;
+        cantidad = 0;
+        
+        if ( productoSeleccionado ){
+            cantidad = parseInt(prompt("Ingrese cantidad"));
+        }else {
+            alert("No existe el producto");
+            otroProducto = confirm("¿Queres agregar otro producto?");
+            if(otroProducto){
                 continue;
-        }
-        let cantidad = parseInt(prompt("Ingresar cantidad"));
-        if (!(cantidad > 0)) {
-            alert("La cantidad debe ser mayor a cero");
-            continue;
-        }
-
-
-        totalConDescuento = totalConDescuento + precioConDescuento * cantidad;
-        totalCarrito= totalCarrito + precio * cantidad;
+            }else{
+                break;
+            }
+        }   
+        productoSeleccionado.incrementar(cantidad);
         otroProducto = confirm("¿Queres agregar otro producto?");
-    } while (otroProducto);
-    alert("Total Del Carrito = $" + totalCarrito + "\nTotal con descuento= $" + totalConDescuento);
+        
+    }while(otroProducto)
+    
 }
+//Funcion para totalizar precio segun productos y cantidad ingresadas
+function calcularTotalCarrito(){
+    for ( productoCarrito of carrito){
+        if ( productoCarrito.cantidad > 0){
+            console.log(productoCarrito.producto.nombre + " Cantidad: " + productoCarrito.cantidad);
+            totalConDescuento = totalConDescuento + productoCarrito.producto.aplicarDescuento() * productoCarrito.cantidad;
+            totalCarrito = totalCarrito + productoCarrito.producto.precio * productoCarrito.cantidad;
+        }
+    }
 
+}
+//Bloque principal
+//Instanciación de objetos tipo Producto(distintas macetas)
+carrito.push(new ItemCarrito(new Producto("Maceta Buda", 300, "Dorado", "Mediano", 0)));
+carrito.push(new ItemCarrito(new Producto("Maceta Gatito", 300, "Gris", "Mediano", 10)));
+carrito.push(new ItemCarrito(new Producto("Maceta Cactus", 200, "Verde", "Pequeño", 0)));
+carrito.push(new ItemCarrito(new Producto("Maceta Llama", 300, "Turquesa", "Mediano",5)));
+
+//Invocamos la funcion que solicita ingresar productos para agregar al carrito
 agregarAlCarrito();
+//Invocamos la funcion que calcula el total del carrito
+calcularTotalCarrito();
+
+alert(`El total de su compra es $${totalCarrito} \nEl total con descuentos es $${totalConDescuento}`);
+
+
+
